3b3eb25ecaa997bf6736b5f05dceee62ea473acc
chesdes <mathewches2008@gmail.com>
2025-08-29 14:56:30 +0300

    fix error in parser and logging parser process

--- utils/parser.py
+++ utils/parser.py
@@ -1,8 +1,23 @@
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 import time
+import requests
+
+import logging
+logger = logging.getLogger(__name__)
+
+def is_valid_image_url(url: str) -> bool:
+        try:
+            response = requests.head(url, timeout=2)
+            content_type = response.headers.get('content-type', '')
+            return response.status_code == 200 and 'image' in content_type.lower()
+        except requests.RequestException:
+            logger.info(f'{url} is invalid')
+            return False
 
 def get_pinterest_images(query: str, limit: int) -> list[str]:
+    start_time = time.time()
+    logger.info(f'started parse {limit} images, of query "{query}"')
     image_urls = []
     max_scrolls = 50
 
@@ -11,7 +26,7 @@ def get_pinterest_images(query: str, limit: int) -> list[str]:
         context = browser.new_context()
         page = context.new_page()
         page.goto("https://www.pinterest.com/search/pins/?q=" + query.replace(" ", "%20"))
-        time.sleep(5)
+        time.sleep(2)
 
         scrolls = 0
         while len(image_urls) < limit and scrolls < max_scrolls:
@@ -21,13 +36,17 @@ def get_pinterest_images(query: str, limit: int) -> list[str]:
                 src = img.get('src')
                 if src and 'pinimg.com' in src and '/236x/' in src:
                     full_src = src.replace("/236x/", "/originals/")
-                    if full_src not in image_urls:
+                    if full_src not in image_urls and is_valid_image_url(full_src):
                         image_urls.append(full_src)
                 if len(image_urls) >= limit:
                     break
-
+            
             if len(image_urls) >= limit:
+                end_time = time.time()
+                logger.info(f'parse finished ({end_time-start_time:.2f} seconds)')
                 break
+            check_time = time.time()
+            logger.info(f'query "{query}" progress: {len(image_urls)}/{limit} ({check_time-start_time:.2f}s)')
 
             page.mouse.wheel(0, 4000)
             time.sleep(2)
